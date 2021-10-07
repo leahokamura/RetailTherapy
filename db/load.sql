9d613eb334aab070a784176799214a8a9c39daca
lcl24 <lcl24@duke.edu>
2021-10-06 23:00:18 -0400

    Update load.sql

--- db/load.sql
+++ db/load.sql
@@ -2,3 +2,4 @@
 \COPY Products FROM 'data/Products.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Purchases FROM 'data/Purchases.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Account FROM 'data/Account.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Product_Reviews FROM 'data/Product_Reviews.csv' WITH DELIMITER ',' NULL '' CSV
