8f2f5c4083ca9319c232982c439f43d8c491465e
lcl24 <lcl24@duke.edu>
2021-11-08 21:11:36 -0500

    Update ProductReviews.py

--- app/models/ProductReviews.py
+++ app/models/ProductReviews.py
@@ -10,21 +10,23 @@ class ProductReviews:
 	self.votes = votes
 
     @staticmethod
-    def get_for_pid(id):
+    def get_all_product_reviews(id):
         rows = app.db.execute('''
-SELECT uid, time_reviewed, rating, comments
+SELECT uid, time_reviewed, rating, comments, votes
 FROM Product_Reviews
 WHERE pid = :pid
+ORDER By votes DESC
 ''',
                               pid=pid)
         return Product_Reviews(*(rows[0])) if rows is not None else None
 
     @staticmethod
-    def get_for_uid(available=True):
+    def get_all_product_reviews_by_user(uid):
         rows = app.db.execute('''
 SELECT pid, time_reviewed, rating, comments
 FROM Product_Reviews
 WHERE uid = :uid
+ORDER BY votes DESC
 ''',
                               uid=uid)
         return [Product(*row) for row in rows]
