bf7dadbd421617f606db5e0fa775f121b56aa033
lcl24 <lcl24@duke.edu>
2021-11-09 11:53:04 -0500

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -15,7 +15,7 @@ class User(UserMixin):
     @staticmethod
     def get_by_auth(email, password):
         rows = app.db.execute("""
-SELECT password, uid, email, firstname, lastname
+SELECT password, id, email, firstname, lastname
 FROM Users
 WHERE email = :email
 """,
