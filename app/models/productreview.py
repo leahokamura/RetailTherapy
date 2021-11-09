9420f344eeeb29c8b724b438593af1637a4c19b7
lcl24 <lcl24@duke.edu>
2021-11-09 13:37:47 -0500

    Update productreview.py

--- app/models/productreview.py
+++ app/models/productreview.py
@@ -22,7 +22,7 @@ WHERE pid = :pid
     @staticmethod
     def get_product_avg_rating(pid):
         rows = app.db.execute('''
-SELECT *, AVG(rating)::numeric(10,2)
+SELECT AVG(rating)::numeric(10,2)
 FROM Product_Reviews
 WHERE pid = :pid
 ''',
