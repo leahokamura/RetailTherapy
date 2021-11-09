1d204dbcce8b0d7fdebf889573dd7c4010cf8f99
lcl24 <lcl24@duke.edu>
2021-11-08 22:13:41 -0500

    Update ProductReviews.py

--- app/ProductReviews.py
+++ app/ProductReviews.py
@@ -2,7 +2,7 @@ from flask import render_template
 from flask_login import current_user
 import datetime
 
-from .models.ProductReviews import ProductReviews
+from .models.productreviews import ProductReviews
 
 from flask import Blueprint
 bp = Blueprint('ProductReviews', __name__)
