d97bc74afe969176066de16bb7d7317f16cd53e9
lcl24 <lcl24@duke.edu>
2021-11-09 12:09:38 -0500

    Update ProductReviews.py

--- app/ProductReviews.py
+++ app/ProductReviews.py
@@ -2,7 +2,7 @@ from flask import render_template
 from flask_login import current_user
 import datetime
 
-from .models.productreview import ProductReviews
+from .models.productreview import ProductReview
 
 from flask import Blueprint
 bp = Blueprint('productreviews', __name__)
@@ -11,7 +11,7 @@ bp = Blueprint('productreviews', __name__)
 @bp.route('/productreviews')
 def ProductReviews():
     # get all reviews for a certain product:
-    p_reviews = ProductReviews.get_all_product_reviews_for_product(1)
+    p_reviews = ProductReview.get_all_product_reviews_for_product(1)
     
     # render the page by adding information to the ProductReviews.html file
     return render_template('ProductReviews.html',
