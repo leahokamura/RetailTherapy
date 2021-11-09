7c96ef68a34a3b29ff9ed6e66f8f6fb3ae3bcfd5
lcl24 <lcl24@duke.edu>
2021-11-08 21:15:52 -0500

    Add new file

---
+++ app/ProductReviews.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from flask_login import current_user
+import datetime
+
+from .models.ProductReviews import ProductReviews
+
+from flask import Blueprint
+bp = Blueprint('ProductReviews', __name__)
+
+
+@bp.route('/')
+def ProductReviews():
+    # get all reviews for a certain product:
+    productreviews = Product.get_all_product_reviews_for_product(pid)
+    
+    # find the products current user has bought:
+    
+    # render the page by adding information to the ProductReviews.html file
+    return render_template('ProductReviews.html',
+                           Product_Reviews=productreviews)
