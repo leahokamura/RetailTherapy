610d53dad1ce78b853f73b9d2180a609cff782a9
lcl24 <lcl24@duke.edu>
2021-11-08 21:31:38 -0500

    Update users.py

--- app/users.py
+++ app/users.py
@@ -68,6 +68,19 @@ def register():
             return redirect(url_for('users.login'))
     return render_template('register.html', title='Register', form=form)
 
+@bp.route('/register1', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        if User.register(form.email.data,
+                         form.password.data,
+                         form.firstname.data,
+                         form.lastname.data):
+            flash('Congratulations, you are now a registered user!')
+            return redirect(url_for('users.login'))
+    return render_template('register1.html', title='Register', form=form)
 
 @bp.route('/logout')
 def logout():
