3fb46987946cf56133598d857439a8a99f15b016
lcl24 <lcl24@duke.edu>
2021-11-09 13:52:04 -0500

    Update productreview.py

--- app/models/productreview.py
+++ app/models/productreview.py
@@ -22,7 +22,7 @@ WHERE pid = :pid
     @staticmethod
     def get_product_avg_rating(pid):
         rows = app.db.execute('''
-SELECT MAX(uid), MAX(pid), MAX(time_reviewed), AVG(rating)::numeric(10,2), MAX(len(comments)), MAX(votes)
+SELECT MAX(uid), MAX(pid), MAX(time_reviewed), AVG(rating)::numeric(10,2), MAX(comments), MAX(votes)
 FROM Product_Reviews
 WHERE pid = :pid
 ''',
