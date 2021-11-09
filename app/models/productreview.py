0f5e4baca14cac9fea19ceabf09eb624665c18ae
lcl24 <lcl24@duke.edu>
2021-11-09 13:03:31 -0500

    Update productreview.py

--- app/models/productreview.py
+++ app/models/productreview.py
@@ -1,9 +1,9 @@
 from flask import current_app as app
 
 class ProductReview:
-    def __init__(self, pid, uid, time_reviewed, rating, comments, votes):
-        self.pid = pid
+    def __init__(self, uid, pid, time_reviewed, rating, comments, votes):
         self.uid = uid
+        self.pid = pid
         self.time_reviewed = time_reviewed
         self.rating = rating
         self.comments = comments
@@ -12,9 +12,9 @@ class ProductReview:
     @staticmethod
     def get_all_product_reviews_for_product(pid):
         rows = app.db.execute('''
-SELECT pid, uid, time_reviewed, rating, comments, votes
+SELECT uid, pid, time_reviewed, rating, comments, votes
 FROM Product_Reviews
-WHERE pid = 1
+WHERE pid = :pid
 ''',
                               pid=pid)
         return [ProductReview(*row) for row in rows]
