2dd85b05ebaac0006a422e56e0d0f3f6aaa98d18
Jonathan Browning <jrb127@duke.edu>
2021-11-11 01:02:00 -0500

    found source of registration issues

--- app/models/user.py
+++ app/models/user.py
@@ -1,3 +1,5 @@
+from __future__ import print_function # In python 2.7
+import sys
 from flask_login import UserMixin
 from flask import current_app as app
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -6,16 +8,17 @@ from .. import login
 
 
 class User(UserMixin):
-    def __init__(self, uid, email, firstname, lastname):
-        self.uid = uid
+    def __init__(self, id, email, firstname, lastname):
+        self.id = id
         self.email = email
         self.firstname = firstname
         self.lastname = lastname
 
+
     @staticmethod
     def get_by_auth(email, password):
         rows = app.db.execute("""
-SELECT password, uid, email, firstname, lastname
+SELECT password, id, email, firstname, lastname
 FROM Users
 WHERE email = :email
 """,
@@ -42,28 +45,29 @@ WHERE email = :email
     def register(email, password, firstname, lastname):
         try:
             rows = app.db.execute("""
-INSERT INTO Users(email, password, firstname, lastname)
-VALUES(:email, :password, :firstname, :lastname)
-RETURNING uid
+INSERT INTO Users(email, firstname, lastname, password)
+VALUES(:email, :firstname, :lastname, :password)
+RETURNING id
 """,
                                   email=email,
-                                  password=generate_password_hash(password),
                                   firstname=firstname,
-                                  lastname=lastname)
-            uid = rows[0][0]
-            return User.get(uid)
+                                  lastname=lastname,
+                                  password=generate_password_hash(password))
+            id = rows[0][0]
+            return User.get(id)
         except Exception:
+            print('made it to the exception', file=sys.stderr)
             # likely email already in use; better error checking and
             # reporting needed
             return None
 
     @staticmethod
     @login.user_loader
-    def get(uid):
+    def get(id):
         rows = app.db.execute("""
-SELECT uid, email, firstname, lastname
+SELECT id, email, firstname, lastname
 FROM Users
-WHERE uid = :uid
+WHERE id = :id
 """,
-                              uid=uid)
+                              id=id)
         return User(*(rows[0])) if rows else None
