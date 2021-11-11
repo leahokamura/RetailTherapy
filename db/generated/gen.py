de82c9e79fac0aa2acdeafe10a64786cb35bff58
Piper Hampsch <ph116@duke.edu>
2021-11-10 21:26:20 -0500

    Coded all data gen functions for all .csv's

--- db/generated/gen.py
+++ db/generated/gen.py
@@ -5,16 +5,18 @@ from faker import Faker
 num_users = 100
 num_products = 2000
 num_purchases = 2500
-num_accounts = num_users
+num_sellers = 100
+num_accounts = num_users + num_sellers
+num_categories = 20
+num_carts = 3000
 
 Faker.seed(0)
 fake = Faker()
 
-
 def get_csv_writer(f):
     return csv.writer(f, dialect='unix')
 
-
+#Users(uid, email, firstname, lastname, address, password)
 def gen_users(num_users):
     with open('Users.csv', 'w') as f:
         writer = get_csv_writer(f)
@@ -34,6 +36,7 @@ def gen_users(num_users):
         print(f'{num_users} generated')
     return
 
+#Account(uid, balance)
 def gen_account(num_accounts):
     with open('Account.csv', 'w') as f:
         writer = get_csv_writer(f)
@@ -46,24 +49,7 @@ def gen_account(num_accounts):
         print(f'{num_accounts} generated')
     return
 
-def gen_products(num_products):
-    available_pids = []
-    with open('Products.csv', 'w') as f:
-        writer = get_csv_writer(f)
-        print('Products...', end=' ', flush=True)
-        for pid in range(num_products):
-            if pid % 100 == 0:
-                print(f'{pid}', end=' ', flush=True)
-            name = fake.sentence(nb_words=4)[:-1]
-            price = f'{str(fake.random_int(max=500))}.{fake.random_int(max=99):02}'
-            available = fake.random_element(elements=('true', 'false'))
-            if available == 'true':
-                available_pids.append(pid)
-            writer.writerow([pid, name, price, available])
-        print(f'{num_products} generated; {len(available_pids)} available')
-    return available_pids
-
-
+#Purchases(uid, oid, time_purchased, total_amount, item_quantity, fulfillment_status, order_page)
 def gen_purchases(num_purchases):
     with open('Purchases.csv', 'w') as f:
         writer = get_csv_writer(f)
@@ -81,9 +67,242 @@ def gen_purchases(num_purchases):
         print(f'{num_purchases} generated')
     return
 
+#Product_Categories(category)
+def gen_product_categories(num_categories):
+    with open('Product_Categories.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Product Categories...', end=' ', flush=True)
+        for categories in range(num_categories):
+            if categories % 10 == 0:
+                print(f'{categories}', end=' ', flush=True)
+            category = fake.sentence(nb_words=2)[:-1]
+            writer.writerow([category])
+        print(f'{num_categories} generated')
+    return
+
+#Products(pid, name, price, available, img)
+def gen_products(num_products):
+    available_pids = []
+    with open('Products.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Products...', end=' ', flush=True)
+        for pid in range(num_products):
+            if pid % 100 == 0:
+                print(f'{pid}', end=' ', flush=True)
+            name = fake.sentence(nb_words=4)[:-1]
+            price = f'{str(fake.random_int(max=500))}.{fake.random_int(max=99):02}'
+            img = fake.image()
+            available = fake.random_element(elements=('true', 'false'))
+            if available == 'true':
+                available_pids.append(pid)
+            writer.writerow([pid, name, price, available, img])
+        print(f'{num_products} generated; {len(available_pids)} available')
+    return available_pids
+
+
+#Cart(cid)
+def gen_cart(num_carts):
+    with open('Cart.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Cart...', end=' ', flush=True)
+        for cid in range(num_carts):
+            if cid % 10 == 0:
+                print(f'{cid}', end=' ', flush=True)
+            writer.writerow([cid])
+        print(f'{num_carts} generated')
+    return
+
+#InCart(cid, p_quantity, unit_price, total_price, pid, uid)
+def gen_in_cart(num_carts):
+    with open('InCart.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('In Cart...', end=' ', flush=True)
+        for cid in range(num_carts):
+            if cid % 100 == 0:
+                print(f'{cid}', end=' ', flush=True)
+            uid = fake.random_int(min=0, max=num_users-1)
+            pid = fake.random_int(min=0, max=num_products-1)
+            p_quantity = f'{str(fake.random_int(max=100))}'
+            unit_price = f'{str(fake.random_int(max=5000))}.{fake.random_int(max=99):02}'
+            total_price = str((int(p_quantity)*int(unit_price)))
+            writer.writerow([cid, p_quantity, unit_price, total_price, pid, uid])
+        print(f'{num_purchases} generated')
+    return
+
+
+#SaveForLater(cid, p_quantity, unit_price, total_price, pid)
+def gen_save_for_later(num_carts, num_products):
+    with open('SaveForLater.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Save For Later...', end=' ', flush=True)
+        for cid in range(num_carts):
+            if cid % 100 == 0:
+                print(f'{cid}', end=' ', flush=True)
+            pid = fake.random_int(min=0, max=num_products-1)
+            p_quantity = f'{str(fake.random_int(max=100))}'
+            unit_price = f'{str(fake.random_int(max=5000))}.{fake.random_int(max=99):02}'
+            total_price = str((int(p_quantity)*int(unit_price)))
+            writer.writerow([cid, p_quantity, unit_price, total_price, pid])
+        print(f'{num_purchases} generated')
+    return
+
+
+#Orders(cid, oid, order_totalPrice, fulfilled)
+def gen_orders(num_purchases, num_carts):
+    with open('Orders.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Orders...', end=' ', flush=True)
+        for cid in range(num_carts):
+            if cid % 100 == 0:
+                print(f'{cid}', end=' ', flush=True)
+            oid = fake.random_int(min=0, max=num_purchases-1)
+            p_quantity = f'{str(fake.random_int(max=100))}'
+            unit_price = f'{str(fake.random_int(max=5000))}.{fake.random_int(max=99):02}'
+            order_totalPrice = str((int(p_quantity)*int(unit_price)))
+            fulfilled = fake.random_element(elements=('true', 'false'))
+            writer.writerow([cid, oid, order_totalPrice, fulfilled])
+        print(f'{num_purchases} generated')
+    return
+
+
+#Sellers(sid)
+def gen_sellers(num_sellers):
+    with open('Sellers.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Sellers...', end=' ', flush=True)
+        for sid in range(num_sellers):
+            if sid % 10 == 0:
+                print(f'{sid}', end=' ', flush=True)
+            writer.writerow([sid])
+        print(f'{num_sellers} generated')
+    return
+
+
+#Inventory(sid, pid, quantity)
+def gen_inventory(num_sellers):
+    with open('Inventory.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Inventory...', end=' ', flush=True)
+        for sid in range(num_sellers):
+            if sid % 10 == 0:
+                print(f'{sid}', end=' ', flush=True)
+            for pid in range(10):
+                quantity = f'{str(fake.random_int(max=100))}'
+                writer.writerow([sid, pid, quantity])
+        print(f'{num_sellers} generated')
+    return
+
+
+#SellerOrders(sid, oid, uid)
+def gen_seller_orders(num_purchases):
+    with open('SellerOrders.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Seller Orders...', end=' ', flush=True)
+        for oid in range(num_purchases):
+            if oid % 10 == 0:
+                print(f'{oid}', end=' ', flush=True)
+            sid = fake.random_int(max=num_sellers)
+            uid = fake.random_int(max=num_users)
+            writer.writerow([sid, oid, uid])
+        print(f'{num_purchases} generated')
+    return
+
+#UpdateSubmission(buyer_balance, seller_balance, fulfilled_time, oid, cid, sid, bid, total_price)
+def gen_update_submission(num_purchases):
+    with open('UpdateSubmission.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Update Submission...', end=' ', flush=True)
+        for oid in range(num_carts):
+            if oid % 100 == 0:
+                print(f'{oid}', end=' ', flush=True)
+            cid = fake.random_int(min=0, max=num_purchases-1)
+            sid = fake.random_int(min=0, max=num_sellers-1)
+            bid = fake.random_int(min=0, max=num_accounts-1)
+            p_quantity = f'{str(fake.random_int(max=100))}'
+            unit_price = f'{str(fake.random_int(max=5000))}.{fake.random_int(max=99):02}'
+            total_price = str((int(p_quantity)*int(unit_price)))
+            fulfilled_time = fake.date_time()
+            buyer_balance = f'{str(fake.random_int(max=5000))}.{fake.random_int(max=99):02}'
+            seller_balance = f'{str(fake.random_int(max=5000))}.{fake.random_int(max=99):02}'
+            writer.writerow([buyer_balance, seller_balance, fulfilled_time, oid, cid, sid, bid, total_price])
+        print(f'{num_purchases} generated')
+    return
+
+
+#Product_Reviews(uid, pid, time, rating, comments, votes)
+def gen_product_reviews(num_products):
+    with open('Product_Reviews.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Product Reviews...', end=' ', flush=True)
+        for pid in range(num_products):
+            if pid % 10 == 0:
+                print(f'{pid}', end=' ', flush=True)
+            uid = fake.random_int(min=0, max=num_users-1)
+            time = fake.date_time()
+            rating = fake.random_int(min=0, max=5)
+            comments = fake.sentence(nb_words=20)[:-1]
+            votes = fake.random_int(min=0, max=num_users-1)
+            writer.writerow([uid, pid, time, rating, comments, votes])
+        print(f'{num_categories} generated')
+    return
+
+
+
+#Seller_Reviews(uid, sid, time, rating, comments, votes)
+def gen_seller_reviews(num_sellers):
+    with open('Seller_Reviews.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Seller Reviews...', end=' ', flush=True)
+        for sid in range(num_sellers):
+            if sid % 10 == 0:
+                print(f'{sid}', end=' ', flush=True)
+            uid = fake.random_int(min=0, max=num_users-1)
+            time = fake.date_time()
+            rating = fake.random_int(min=0, max=5)
+            comments = fake.sentence(nb_words=20)[:-1]
+            votes = fake.random_int(min=0, max=num_users-1)
+            writer.writerow([uid, sid, time, rating, comments, votes])
+        print(f'{num_categories} generated')
+    return
+
+
+#Images_Reviews(uid, pid, img)
+def gen_images_reviews(num_products):
+    with open('Images_Reviews.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Images Reviews...', end=' ', flush=True)
+        for pid in range(num_products):
+            if pid % 10 == 0:
+                print(f'{pid}', end=' ', flush=True)
+            uid = fake.random_int(min=0, max=num_users-1)
+            img = fake.image()
+            writer.writerow([uid, pid, img])
+        print(f'{num_categories} generated')
+    return
+
 
 gen_users(num_users)
 gen_account(num_accounts)
-available_pids = gen_products(num_products)
 gen_purchases(num_purchases)
+
+gen_product_categories(num_categories)
+available_pids = gen_products(num_products)
+
+gen_cart(num_carts)
+gen_in_cart(num_carts)
+gen_save_for_later(num_carts, num_products)
+gen_orders(num_purchases, num_carts)
+
+gen_sellers(num_sellers)
+gen_inventory(num_sellers)
+gen_seller_orders(num_purchases)
+gen_update_submission(num_purchases)
+
+gen_product_reviews(num_products)
+gen_seller_reviews(num_sellers)
+gen_images_reviews(num_products)
+
+
+
+
 
