d1a370eae2d47875ff8dd6dd9cf7e0ca28f3dd86
Jonathan Browning <jrb127@duke.edu>
2021-11-21 15:21:21 -0500

    support using multiple keywords to search; prevent duplicates in results

--- app/ProductSearch.py
+++ app/ProductSearch.py
@@ -24,7 +24,23 @@ def ProductSearch(category):
 def ProductKeywordSearch(keywords):
     print('these are the keywords that we are searching for', file=sys.stderr)
     print(keywords, file=sys.stderr)
-    products = Product.get_by_keyword(keywords)
+    keywords = keywords.strip()
+    keywords = list(keywords.split(" "))
+    print('these are the keywords that we are searching for again', file=sys.stderr)
+    print(keywords, file=sys.stderr)
+    
+    products = []
+    prod_ids = []
+    for word in keywords:
+        tempList = Product.get_by_keyword(word)
+        for prod in tempList:
+            print('this is the product in question', file=sys.stderr)
+            print(prod.pid, file = sys.stderr)
+            if prod.pid not in prod_ids:
+                products.append(prod)
+                prod_ids.append(prod.pid)
+    # products = Product.get_by_keyword(keywords)
+    print('these are the products ', file=sys.stderr)
     print(products, file=sys.stderr)
     return render_template('prod-search.html',
                             category = 'none',
