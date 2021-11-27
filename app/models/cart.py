dd0b2a3f9a61e198cccf29da34aef5c15838c9d0
Leah Okamura <leo12@duke.edu>
2021-11-26 21:29:17 -0500

    fixed cart page PLZZZ

--- app/models/cart.py
+++ app/models/cart.py
@@ -2,58 +2,26 @@ from flask import current_app as app
 
 
 class Cart:
-    def __init__(self, cid, p_quantity, unit_price, total_price, pid, uid):
-        self.cid = cid
+    def __init__(self, uid, pid, name, p_quantity, unit_price, seller_id):
+        self.uid = uid
+        self.pid = pid
+        self.name = name
         self.p_quantity = p_quantity
+        self.unit_price = unit_price
+        self.seller_id = seller_id
+    
 
     @staticmethod
     def get_cart(uid):
         rows = app.db.execute('''
-SELECT cid, p_quantity, unit_price, total_price, pid, uid
+SELECT uid, pid, name, p_quantity, unit_price, seller_id
 FROM InCart
 WHERE uid = :uid
 ''',
                               uid=uid)
+        print("got cart data")
+        print([Cart(*row) for row in rows])
         return [Cart(*row) for row in rows] if rows is not None else None
-
-
-#leah is also working on this ://
-#     @staticmethod
-#     def update(pid, uid, option):
-
-#         rows = app.db.execute('''
-# SELECT p_quantity
-# FROM InCart
-# WHERE uid = :uid AND pid = :pid
-# ''', uid=uid, pid=pid)
-
-#     current = int(rows[0][0])
-#     updated = current + 1
-
-# app.db.execute('''
-# UPDATE Cart
-# SET p_quantity = :updated
-# WHERE id = :uid AND pid = :pid
-# ''', uid=uid, pid=pid, updated=updated)
-#         except Exception as e:
-#             print(e)
-
-# @staticmethod
-#     def add(pid, uid):
-#     app.db.execute(
-#     '''
-#         INSERT INTO InCart VALUES(:uid, :pid, :name, 1, :unit_price, :seller_id))
-#     ''',                            
-#                                       id = id, 
-#                                       uid = uid
-#             )
-#         except Exception as e:
-#             print(e)
- 
-
-
-
-
 
 #     @staticmethod
 #     def get(pid):
