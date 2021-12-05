07237e1ab9f8996725ca395f335ece49862f2b63
Lucas Lynn <lcl24@duke.edu>
2021-12-05 13:06:13 -0500

    commenting code

--- app/AddSellerReviews.py
+++ app/AddSellerReviews.py
@@ -4,35 +4,35 @@ from flask_login import current_user
 import datetime
 import sys
 
+#import forms
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, FloatField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, NumberRange
 from flask_babel import _, lazy_gettext as _l
 
-
+#import models
 from .models.seller import Seller
 from .models.sellerreview import SellerReview
 
 from flask import current_app as app
 
 from flask import Blueprint
-# import sys
 bp = Blueprint('addsellerreviews', __name__)
 
-
+#form to add review for a seller
 class AddReviewForm(FlaskForm):
     rating = FloatField(_l('Rating'), validators=[DataRequired(), NumberRange(min=0, max=5)])
     comment = TextAreaField(_l('Comment'), validators=[DataRequired()])
     submit = SubmitField(_l('Add Review'))
 
-
+#routes to form to add a seller review
 @bp.route('/addsellerreview/seller<int:sid>/reviewer<int:rid>', methods=['GET', 'POST'])
 def addSellerReviews(sid, rid):
     if current_user.is_authenticated:
         form = AddReviewForm()
-        print('check 1')
+        print('check 1:user authenticated')
         if form.validate_on_submit():
-            print('made it this far', file=sys.stderr)
+            print('form validated on submit', file=sys.stderr)
             default_time = datetime.datetime.now()
             default_time = datetime.datetime.strftime(default_time, '%Y-%m-%d %H:%M:%S')
             if SellerReview.addreview(rid,
@@ -41,11 +41,10 @@ def addSellerReviews(sid, rid):
                                         form.rating.data,
                                         form.comment.data,
                                         0):
-                print('made it into the if statement', file=sys.stderr)
-                #flash('Congratulations, you have submitted a review!')
+                print('seller review added', file=sys.stderr)
                 return redirect(url_for('sellerreviews.SellerReviews', seller_id = sid))
             else:
-                print('baddddd', file=sys.stderr)
+                print('Error: seller review not added', file=sys.stderr)
     
     seller_review_stats = SellerReview.get_stats(sid)
 
@@ -55,19 +54,20 @@ def addSellerReviews(sid, rid):
                                                     sellerinfo = seller_name,
                                                     sellerreviewstats = seller_review_stats)
 
+#form to edit existing seller review by current user
 class EditReviewForm(FlaskForm):
     rating = FloatField(_l('Rating'), validators=[DataRequired(), NumberRange(min=0, max=5)])
     comment = TextAreaField(_l('Comment'), validators=[DataRequired()])
     submit = SubmitField(_l('Edit Review'))
 
-
+#routes to form to edit existing seller review by current user
 @bp.route('/editsellerreview/seller<int:sid>/reviewer<int:rid>', methods=['GET', 'POST'])
 def editSellerReviews(sid, rid):
     if current_user.is_authenticated:
         form = EditReviewForm()
-        print('check 1')
+        print('check 1: user authenticated')
         if form.validate_on_submit():
-            print('made it this far', file=sys.stderr)
+            print('form validated on submit', file=sys.stderr)
             default_time = datetime.datetime.now()
             default_time = datetime.datetime.strftime(default_time, '%Y-%m-%d %H:%M:%S')
             if SellerReview.editreview(rid,
@@ -76,11 +76,10 @@ def editSellerReviews(sid, rid):
                                         form.rating.data,
                                         form.comment.data,
                                         0):
-                print('made it into the if statement', file=sys.stderr)
-                #flash('Congratulations, you have submitted a review!')
+                print('seller review updated', file=sys.stderr)
                 return redirect(url_for('sellerreviews.SellerReviews', seller_id = sid))
             else:
-                print('baddddd', file=sys.stderr)
+                print('Error: seller review not updated', file=sys.stderr)
     
     seller_review_stats = SellerReview.get_stats(sid)
 
